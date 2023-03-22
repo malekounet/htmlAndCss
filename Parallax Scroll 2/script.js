357a728b5d6d9a2fa751ab415873bfa310559219
malekounet <malekounet@gmail.com>
2023-03-22 16:56:02 +0000

    ...

--- Parallax Scroll 2/script.js
+++ Parallax Scroll 2/script.js
@@ -1,3 +1,11 @@
+let atlas = document.getElementById("atlas");
+
+
+let style = window.getComputedStyle(atlas);
+let tops = style.getPropertyValue("top");
+tops = parseInt(tops)
+
+// console.log(document.styleSheets[0].rules[4].style.setProperty("top", "700px"))
 window.addEventListener("scroll", function(){
-    console.log(this.window.scrollY);
+    document.styleSheets[0].rules[4].style.setProperty("top", (tops - this.window.scrollY*0.4) + "px")
 });
